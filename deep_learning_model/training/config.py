dfe5ac6206cba343b919e5131e40ba7f488e37ac
Jaber <jabertuhin@gmail.com>
2020-06-08 02:20:59 +0600

    add config file

---
+++ deep_learning_model/training/config.py
@@ -0,0 +1,18 @@
+import torchvision.transforms as transforms
+from torchvision.models import resnet34
+
+TRANSFORM = transforms.Compose(
+    [transforms.ToTensor(),
+     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+TRAIN_BATCH_SIZE = 32     
+TEST_BATCH_SIZE = 4
+VAL_BATCH_SIZE = 4
+MODEL = resnet34(pretrained=True)
+NUMBER_OF_WORKERS = 2
+NUM_CLASSES = 10
+LEARNING_RATE = 10e-3
+CLASSES = ('plane', 'car', 'bird', 'cat',
+           'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+
+MODEL_NAME = 'ResNet34model.pth'
