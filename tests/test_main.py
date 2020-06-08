b0288e70cbf604a1b1296b0e5478ff7ca89684ce
Jaber <jabertuhin@gmail.com>
2020-06-08 15:16:36 +0600

    simplify filepath to read file

--- tests/test_main.py
+++ tests/test_main.py
@@ -10,8 +10,8 @@ def test_root(test_app):
 
 
 def test_predict_when_image_is_valid(test_app):
-    # arrange         
-    filepath = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'assets', 'cat_image.jpg')     
+    # arrange             
+    filepath = os.path.join('tests', 'assets', 'cat_image.jpg')     
     expected_response = {'filename': 'cat_image.jpg', 'contentype': 'image/jpeg'} ## won't check predicted class
     # act
     actual_response = test_app.post('/predict/', files={"file": ("cat_image.jpg", open(filepath, "rb"), "image/jpeg")})        
@@ -23,8 +23,8 @@ def test_predict_when_image_is_valid(test_app):
     assert actual_response['contentype'] == expected_response['contentype']    
 
 def test_predict_when_file_not_an_image(test_app):
-    # arrange         
-    filepath = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'assets', 'not_image.txt')     
+    # arrange             
+    filepath = os.path.join('tests', 'assets', 'not_image.txt')     
     expected_response = {'detail' :'File \'not_image.txt\' is not an image.'}
     # act
     actual_response = test_app.post('/predict/', files={"file":  open(filepath, "rb")})
