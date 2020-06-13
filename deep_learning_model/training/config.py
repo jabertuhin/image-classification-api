f5be5db70af6c08acc145042e2f5c5601e6f5c47
Jaber <jabertuhin@gmail.com>
2020-06-13 15:08:15 +0600

    Modify config file.

--- deep_learning_model/training/config.py
+++ deep_learning_model/training/config.py
@@ -5,11 +5,10 @@ TRANSFORM = transforms.Compose(
     [transforms.ToTensor(),
      transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
-TRAIN_BATCH_SIZE = 32     
+TRAIN_BATCH_SIZE = 32
 TEST_BATCH_SIZE = 4
 VAL_BATCH_SIZE = 4
-MODEL = resnet34(pretrained=True)
-NUMBER_OF_WORKERS = 2
+MODEL = resnet34(pretrained=False) ## change it to True before training
 NUM_CLASSES = 10
 LEARNING_RATE = 10e-3
 CLASSES = ('plane', 'car', 'bird', 'cat',
