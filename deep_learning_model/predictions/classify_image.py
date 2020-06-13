e5e050d893d0ec6d2e5e5f7f2c3fc980b3be3d82
Jaber <jabertuhin@gmail.com>
2020-06-13 16:27:59 +0600

    Create ImageClassifier class.

--- deep_learning_model/predictions/classify_image.py
+++ deep_learning_model/predictions/classify_image.py
@@ -1,25 +1,26 @@
 from deep_learning_model.training.models import Classifier
 from deep_learning_model.training.config import CLASSES, MODEL_NAME
 
-
 import torch
 import torchvision.transforms as transforms
 
 import os
 
-def prediction(image):    
-    classifier = Classifier()
-    model_path = os.path.join('deep_learning_model', 'trained_model', MODEL_NAME)         
-    classifier.load_state_dict(torch.load(model_path, map_location=torch.device('cpu')))
+class ImageClassifier:
+    def __init__(self):        
+        self.classifier = Classifier()
+        model_path = os.path.join('deep_learning_model', 'trained_model', MODEL_NAME)                 
+        self.classifier.load_state_dict(torch.load(model_path, map_location=torch.device('cpu')))
 
-    transforms_image = transforms.Compose(
-        [transforms.Resize((224, 224)),
-        transforms.ToTensor(),        
-        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
-    image = transforms_image(image) 
-    image = image.unsqueeze(0)
+    def predict(self, image):            
+        transforms_image = transforms.Compose(
+            [transforms.Resize((224, 224)),
+            transforms.ToTensor(),        
+            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+        image = transforms_image(image) 
+        image = image.unsqueeze(0)
 
-    output = classifier(image)    
-    class_idx = torch.argmax(output, dim=1)    
-    
-    return CLASSES[class_idx]
+        output = self.classifier(image)    
+        class_idx = torch.argmax(output, dim=1)    
+        
+        return CLASSES[class_idx]
