140fbc4bd6716d0f091f9a59c272ce1f4befc061
Jaber <jabertuhin@gmail.com>
2020-06-13 15:09:44 +0600

    Add seed value and comments.

--- deep_learning_model/training/train.py
+++ deep_learning_model/training/train.py
@@ -4,9 +4,14 @@ import pytorch_lightning as pl
 from deep_learning_model.training.models import Classifier
 from deep_learning_model.training import config
 
+pl.seed_everything(42)
+
 model = Classifier()
 trainer = pl.Trainer(max_epochs=50, gpus=1)
 
+# start training
 trainer.fit(model)
+# save trained model
 torch.save(model.state_dict(), config.MODEL_NAME)
+# test on test data
 trainer.test(model)
